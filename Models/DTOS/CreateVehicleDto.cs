27e0cdb29c903e0b2eb04af8af8e21c6a3671bf9
hassan <davidlopezolivares05@gmail.com>
2025-11-17 00:42:47 -0400

    feat: avances

---
+++ Models/DTOS/CreateVehicleDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Security.Models.DTOS
+{
+    public class CreateVehicleDto
+    {
+        public string Brand { get; set; } = default!;
+        public string Model { get; set; } = default!;
+        public string Plate { get; set; } = default!;
+        public int Year { get; set; }
+        public Guid DriverId { get; set; }    // Conductor asignado
+        public string Status { get; set; } = "Activo";
+    }
+}
---
+++ Models/DTOS/UpdateVehicleDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Security.Models.DTOS
+{
+    public class UpdateVehicleDto
+    {
+        public string Brand { get; set; } = default!;
+        public string Model { get; set; } = default!;
+        public string Plate { get; set; } = default!;
+        public int Year { get; set; }
+        public Guid DriverId { get; set; }
+        public string Status { get; set; } = "Activo";
+    }
+}
