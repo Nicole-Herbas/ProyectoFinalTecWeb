3a36538363b9dffb0340096ef92665f865790686
hassan <davidlopezolivares05@gmail.com>
2025-11-21 16:28:25 -0400

    avance vehiculos

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,26 @@
-﻿namespace ProyectoFinal.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Security.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using TaxiApi.Models;
+
+namespace TaxiApi.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // Por ahora, SOLO Vehículos
+        public DbSet<Vehicle> Vehicles => Set<Vehicle>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Placa única
+            modelBuilder.Entity<Vehicle>()
+                .HasIndex(v => v.Plate)
+                .IsUnique();
+        }
     }
 }
--- Models/Vehiculo.cs
+++ Models/Vehiculo.cs
@@ -1,6 +1,30 @@
-﻿namespace ProyectoFinal.Models
+﻿using ProyectoFinal.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Security.Models
 {
-    public class Vehiculo
+    public class Vehicle
     {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required, MaxLength(80)]
+        public string Brand { get; set; } = default!;   // marca
+
+        [Required, MaxLength(80)]
+        public string Model { get; set; } = default!;   // modelo
+
+        [Required, MaxLength(15)]
+        public string Plate { get; set; } = default!;   // placa
+
+        [Range(1980, 2100)]
+        public int Year { get; set; }                   // año
+
+        [Required]
+        public Guid DriverId { get; set; }              // FK → User (Conductor)
+        public User? Driver { get; set; }
+
+        [Required, MaxLength(20)]
+        public string Status { get; set; } = "Activo";  // Activo / Inactivo
     }
 }
