5320165e2a93a8be2b77b1b29a9c39c97c4a7963
Herbas-n <nicole.herbas@ucb.edu.bo>
2025-11-25 23:25:00 -0400

    feat: adding viaje repository

--- Repositories/IViajeRepository.cs
+++ Repositories/IViajeRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace ProyectoFinal.Repositories
+﻿using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Repositories
 {
     public interface IViajeRepository
     {
+        Task AddAsync(Viaje viaje);
+        Task<Viaje?> GetViajeAsync(Guid id);
+        Task<bool> ExistsAsync(Guid id);
+        Task<int> SaveChangesAsync();
     }
 }
