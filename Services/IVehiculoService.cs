3d62a09273bccc754bc707cb7186152237c29cac
hassan <davidlopezolivares05@gmail.com>
2025-11-26 03:16:41 -0400

    feat: implement vehiculo service logic

---
+++ Services/IVehiculoService.cs
@@ -0,0 +1,14 @@
+﻿using ProyectoFinal.Models.DTOS;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Services
+{
+    public interface IVehiculoService
+    {
+        Task<Guid> CreateAsync(CreateVehicleDto dto);
+        Task<Vehiculo?> GetByIdAsync(Guid id);
+        Task<IEnumerable<Vehiculo>> GetAllAsync();
+        Task<bool> UpdateAsync(Guid id, UpdateVehicleDto dto);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
