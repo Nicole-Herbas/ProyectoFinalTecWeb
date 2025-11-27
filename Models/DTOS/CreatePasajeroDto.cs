0c6516a27400d0f2192ba048c465f4ff3611c1a6
Herbas-n <nicole.herbas@ucb.edu.bo>
2025-11-27 19:34:29 -0400

    feat: pasajero service

---
+++ Models/DTOS/CreatePasajeroDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinal.Models.DTOS
+{
+    public class CreatePasajeroDto
+    {
+        [Required]
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = "Pasajero";
+    }
+}
---
+++ Models/DTOS/UpdatePasajeroDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoFinal.Models.DTOS
+{
+    public class UpdatePasajeroDto
+    {
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+    }
+}
